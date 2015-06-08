018b147383db399bf50e3422efb70d9731aa12cf
acivux <acivux@gmail.com>
2015-06-08 16:44:30 -0700

    u4

--- cleanup.sql
+++
@@ -1,5 +0,0 @@
-DELETE FROM tournament_matches;
-DELETE FROM tournament_players;
-DELETE FROM matches;
-DELETE FROM player;
-DELETE FROM tournament;
--- reset.sql
+++
@@ -1,2 +0,0 @@
-DROP DATABASE tournament;
-CREATE DATABASE tournament;
